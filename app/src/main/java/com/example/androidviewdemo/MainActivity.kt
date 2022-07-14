df16ccdf5669e04a597834c6e7bd944362174fe4
Vladislav Trofimov <gthbttr@gmail.com>
2022-07-15 01:12:24 +0300

    dev

--- app/src/main/java/com/example/androidviewdemo/MainActivity.kt
+++ app/src/main/java/com/example/androidviewdemo/MainActivity.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.androidviewdemo.databinding.ActivityMainBinding
 
 @SuppressLint("SetTextI18n")
 class MainActivity : AppCompatActivity() {
@@ -16,7 +18,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         //demoSimpleInterface()
-        demoComplexInterface()
+        //demoComplexInterface()
+        //demoSimpleLayoutInflater()
+        //demoSimpleLayoutInflater()
+        //demoComplexLayoutInflater()
+        demoViewBinding()
 
     }
 
@@ -98,6 +104,62 @@ class MainActivity : AppCompatActivity() {
         button.layoutParams = layoutParams
 
         return button
+
+    }
+
+    private fun demoSimpleLayoutInflater() {
+
+        // get LayoutInflater from Main Activity context
+        val inflater = this@MainActivity.layoutInflater
+
+        // inflate view from text view xml file
+        // without any rout
+        val textView = inflater.inflate(R.layout.text_view, null)
+
+        // set created view as content of Main Activity
+        this@MainActivity.setContentView(textView)
+
+    }
+
+    private fun demoComplexLayoutInflater() {
+
+        // get LayoutInflater from Main Activity context
+        val inflater = this@MainActivity.layoutInflater
+
+        // inflate LinearLayout from xml file (layout contains one textview)
+        val linearLayout: LinearLayoutCompat = inflater.inflate(
+            R.layout.linear_layout, null) as LinearLayoutCompat
+
+        // inflate additional textView from xml file
+        val secondTextView = inflater.inflate(R.layout.text_view, null)
+        // add additional text view to our linear layout through addView() method
+        linearLayout.addView(secondTextView)
+
+        // inflate additional textView from xml file and add this view to existing
+        // layout with params root: ViewGroup and attachToRoot: Boolean
+        inflater.inflate(R.layout.text_view, linearLayout, true)
+
+        // set created layout (with textview inside) as content of Main Activity
+        this@MainActivity.setContentView(linearLayout)
+
+    }
+
+    private fun demoViewBinding() {
+
+        // get LayoutInflater from Main Activity context
+        val inflater = this@MainActivity.layoutInflater
+
+        // inflate binding with inflater
+        // this makes object of generated binding class from xml file
+        val binding = ActivityMainBinding.inflate(inflater)
+
+        // now we have access to generated views which have id's
+        binding.tvFirst.text = "Hello!"
+        binding.tvSecond.text = "Thank u for visiting my github!"
+        binding.tvThird.text = "Looking for job! =)"
+
+        // use binding.root to get root layout
+        setContentView(binding.root)
 
     }
 
